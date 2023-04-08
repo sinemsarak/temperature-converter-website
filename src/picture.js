7c12f31f2494414b6cf7d3e8b86607c14bc20ae2
sinemsarak <snmsrkss@gmail.com>
2023-04-08 21:00:55 +0300

    Update picture.js

--- src/picture.js
+++ src/picture.js
@@ -5,7 +5,7 @@ if (x<15){
     var durum= "https://i.imgflip.com/1wtb6f.jpg?a465888"
 }
 else if (x<30){
-    var durum= "https://thumbs.dreamstime.com/z/cute-smiling-sun-icon-vector-illustration-149624044.jpg"
+    var durum= "https://assets.ytv.com/wp-content/uploads/2017/10/15121231/YTV_SB_Best_Year_Ever_Tile.jpg"
 }
 else{
     var durum= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlYTZiVSzu8rcAgQ9epSHXPIqz9SO8w1sufW5b5F7RiqCenw2H"
