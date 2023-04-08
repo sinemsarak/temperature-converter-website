7994319cf8a2fbff4c70ea6da390fd4ee509e895
sinemsarak <snmsrkss@gmail.com>
2023-04-08 21:00:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,133 +7,138 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { Button } from "reactstrap";
 import Picture from "./picture";
-import { Component } from "react";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sicak: 25,
-    };
-  }
-
-  artir = () => {
-    this.setState({
-      sicak: this.state.sicak + 1,
-    });
-  };
-  azalt = () => {
-    this.setState({
-      sicak: this.state.sicak - 1,
-    });
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          backgroundColor: "#80ceff",
-          height: "100vh",
-          width: "100%",
-        }}
-      >
-        <Container>
-          <Row>
-            <Col
-              xs="8"
+function App() {
+  const [deger, setDeger] = useState("25");
+  const degernumber = Number(deger);
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "auto",
+        display: "flex",
+        backgroundColor: "#80ceff",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col
+            xs="8"
+            style={{
+              margin: "20px",
+              marginLeft: "40px",
+              marginRight: "40px",
+              marginBottom: "20px",
+              borderRadius: "10px",
+            }}
+          >
+            <div
               style={{
-                margin: "20px",
-                marginLeft: "40px",
-                marginRight: "40px",
-                marginTop: "70px",
-                marginBottom: "20px",
+                backgroundImage: `url(https://swall.teahub.io/photos/small/2-27230_clouds-background-hd-sky-hd-images-png.jpg)`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
+                height: 280,
+                width: 498,
+
+                paddingTop: "20px",
+                paddingLeft: "20px",
+                paddingRight: "20px",
+                paddingBottom: "20px",
+                borderRadius: "10px",
+                boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.5)",
+              }}
+            >
+              <Container style={{ color: " #003366", fontWeight: "bold" }}>
+                <Row>
+                  <Col>
+                    <h2>Hava Sıcaklığı:</h2>
+                    <br />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p>
+                      <label>Şu an sıcaklık</label> {"  "}
+                      <input
+                        id="girdi"
+                        value={deger}
+                        onChange={(a) => setDeger(a.target.value)}
+                        type="number"
+                        style={{
+                          backgroundColor: "transparent",
+                          color: "white",
+                          width: "40px",
+                          borderStyle: "solid",
+                          borderColor: "black",
+                          textAlign: "center"
+                        }}
+                      />
+                      {"  "}
+                      <label>derece</label>
+                    </p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs="4">
+                    <br />
+                    <Button
+                      size="sm"
+                      outline
+                      color="danger"
+                      onClick={() => setDeger(degernumber + 1)}
+                    >
+                      {" "}
+                      Sıcaklığı 1 derece arttır
+                    </Button>
+                  </Col>
+                  <Col xs="4">
+                    <br />
+                    <Button
+                      size="sm"
+                      outline
+                      color="primary"
+                      onClick={() => setDeger(degernumber - 1)}
+                    >
+                      {" "}
+                      Sıcaklığı 1 derece azalt
+                    </Button>{" "}
+                    <br></br> <br></br>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs="4">
+                    <Celcius derece={degernumber} />
+                  </Col>
+                  <Col xs="4">
+                    <Fhr derece={degernumber} />
+                  </Col>
+                  <Col xs="4">
+                    <Kelvin derece={degernumber} />
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </Col>
+          <Col xs="3">
+            <div
+              style={{
+                marginRight: "60px",
+                paddingLeft: "20px",
+                paddingRight: "20px",
+                paddingBottom: "20px",
                 borderRadius: "10px",
               }}
             >
-              <div
-                style={{
-                  backgroundImage: `url(https://swall.teahub.io/photos/small/2-27230_clouds-background-hd-sky-hd-images-png.jpg)`,
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "contain",
-                  height: 280,
-                  width: 498,
-
-                  paddingTop: "20px",
-                  paddingLeft: "20px",
-                  paddingRight: "20px",
-                  paddingBottom: "20px",
-                  borderRadius: "10px",
-                  boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.5)",
-                }}
-              >
-                <Container style={{ color: " #003366", fontWeight: "bold" }}>
-                  <Row>
-                    <Col>
-                      <h2>Hava Sıcaklığı:</h2>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <p>Şu an sıcaklık {this.state.sicak} derece</p>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col xs="4">
-                      <Button
-                        size="sm"
-                        outline
-                        color="danger"
-                        onClick={this.artir}
-                      >
-                        {" "}
-                        Sıcaklığı 1 derece arttır
-                      </Button>
-                    </Col>
-                    <Col xs="4">
-                      <Button
-                        size="sm"
-                        outline
-                        color="primary"
-                        onClick={this.azalt}
-                      >
-                        {" "}
-                        Sıcaklığı 1 derece azalt
-                      </Button>{" "}
-                      <br></br> <br></br>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col xs="4">
-                      <Celcius derece={this.state.sicak} />
-                    </Col>
-                    <Col xs="4">
-                      <Fhr derece={this.state.sicak} />
-                    </Col>
-                    <Col xs="4">
-                      <Kelvin derece={this.state.sicak} />
-                    </Col>
-                  </Row>
-                </Container>
-              </div>
-            </Col>
-            <Col xs="3">
-              <div
-                style={{
-                  marginTop: "60px",
-                  marginRight: "60px",
-                  paddingTop: "20px",
-                  paddingLeft: "20px",
-                  paddingRight: "20px",
-                  paddingBottom: "20px",
-                  borderRadius: "10px",
-                }}
-              >
-                <Picture derece={this.state.sicak} />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+              <Picture derece={degernumber} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
+
+export default App;
